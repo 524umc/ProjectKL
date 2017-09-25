078d8873d2f94002d259f7a25060d855786e91c6
Novaq <524umc@gmail.com>
2017-09-25 19:27:16 +0200

    first commit

---
+++ src/kl/Main.java
@@ -0,0 +1,22 @@
+package kl;
+
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+public class Main implements NativeKeyListener{
+
+	@Override
+	public void nativeKeyPressed(NativeKeyEvent arg0) {
+		
+	}
+
+	@Override
+	public void nativeKeyReleased(NativeKeyEvent arg0) {
+		
+	}
+
+	@Override
+	public void nativeKeyTyped(NativeKeyEvent arg0) {
+		
+	}
+}
